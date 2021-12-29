855788bae6806b8a520e772b1c3c560a3bc19543
isaak-a <isaakranderson@gmail.com>
2021-12-29 16:37:53 -0500

    Consolidate models to db.py

--- classify/db.py
+++ classify/db.py
@@ -1,6 +1,15 @@
 import os
 
-import sqlalchemy as sa
+from sqlalchemy import (
+    MetaData,
+    Table,
+    Column,
+    Text,
+    Integer,
+    Date,
+    BigInteger,
+    create_engine
+)
 
 
 USER = 'jupyter'
@@ -11,22 +20,65 @@ DB = 'expunge'
 
 DATABASE_URI = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 
+meta = MetaData()
 
 EXPUNGE_TABLE = 'expunge_clean' # Full Dataset
 # EXPUNGE_TABLE = 'expunge_10k_clean' # ~26K records
 # EXPUNGE_TABLE = 'expunge_1k_clean' # ~2.6K records
 
+expunge = Table(EXPUNGE_TABLE, meta,
+    Column('record_id', BigInteger, primary_key=True),
 
-
-expunge_model = sa.Table(EXPUNGE_TABLE, sa.MetaData(),
-    sa.Column('person_id', sa.Integer),
-    sa.Column('HearingDate', sa.DateTime),
-    sa.Column('CodeSection', sa.String),
-    sa.Column('ChargeType', sa.String),
-    sa.Column('Class', sa.String),
-    sa.Column('DispositionCode', sa.String),
-    sa.Column('Plea', sa.String),
-    sa.Column('Race', sa.String),
-    sa.Column('Sex', sa.String),
-    sa.Column('fips', sa.Integer),
+    Column('person_id', BigInteger),
+    Column('HearingDate', Date),
+    Column('CodeSection', Text),
+    Column('ChargeType', Text),
+    Column('Class', Text),
+    Column('DispositionCode', Text),
+    Column('Plea', Text),
+    Column('Race', Text),
+    Column('Sex', Text),
+    Column('fips', Integer),
 )
+
+expunge_features = Table('expunge_features', meta,
+    Column('record_id', BigInteger, primary_key=True),
+
+    Column('person_id', BigInteger),
+    Column('HearingDate', Date),
+    Column('CodeSection', Text),
+    Column('ChargeType', Text),
+    Column('Class', Text),
+    Column('DispositionCode', Text),
+    Column('Plea', Text),
+    Column('Race', Text),
+    Column('Sex', Text),
+    Column('fips', Integer),
+    
+    Column('disposition', Text),
+    Column('chargetype', Text),
+    Column('codesection', Text),
+    Column('convictions', Integer),
+
+    Column('last_hearing_date', Date),
+    Column('last_felony_conviction_date', Date),
+    Column('next_conviction_date', Date),
+
+    Column('last_hearing_delta', Integer),
+    Column('last_felony_conviction_delta', Integer),
+    Column('next_conviction_delta', Integer),
+    Column('from_present_delta', Integer),
+
+    Column('arrest_disqualifier', Integer),
+    Column('felony_conviction_disqualifier', Integer),
+    Column('next_conviction_disqualifier_short', Integer),
+    Column('next_conviction_disqualifier_long', Integer),
+    Column('from_present_disqualifier_short', Integer),
+    Column('from_present_disqualifier_long', Integer),
+    Column('class1_2', Integer),
+    Column('class3_4', Integer),
+)
+
+if __name__ == '__main__':
+    engine = create_engine(DATABASE_URI, echo=True)
+    meta.create_all(engine)
