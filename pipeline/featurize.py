f54fefedcc771b0cf7e1e53425d95e71b5240e38
isaak-a <isaakranderson@gmail.com>
2022-02-23 20:59:11 -0500

    Fix more imports

--- pipeline/featurize.py
+++ pipeline/featurize.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import dask.dataframe as dd
 
-from pipeline.config import ExpungeConfig
+from pipeline.expunge_config import ExpungeConfig
 
 
 logger = logging.getLogger(__name__)
