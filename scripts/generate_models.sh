4a30bf197f4b0bc62640f10eb00eef617679d0ba
isaak-a <isaakranderson@gmail.com>
2022-02-27 13:49:22 -0500

    Generate models script revisions

--- scripts/generate_models.sh
+++ scripts/generate_models.sh
@@ -4,10 +4,11 @@
 # Uses DBML CLI to translate DBML -> SQL, then O!MyModels to 
 # translate SQL -> models.py
 
+set -eu
+
 DBML_FILE=../va_courts.dbml
 MODELS_LOCATION=../db # SQLAlchemy models file directory
 TEMP_FILE=/tmp/models.sql # Location for writing intermediate .sql file
-
 
 # Check for script requirements
 if ! command -v dbml2sql &> /dev/null; then
@@ -22,11 +23,17 @@ if ! command -v omm &> /dev/null; then
     exit
 fi
 
+CWD=$PWD
+SCRIPT_PATH=${0%/*}
+if [ "$0" != "$SCRIPT_PATH" ] && [ "$SCRIPT_PATH" != "" ]; then 
+    cd "$SCRIPT_PATH" || exit
+fi
 
-echo "Converting $DBML_FILE to SQL"
+DBML_FILENAME=$(basename $DBML_FILE)
+echo "Converting ${DBML_FILENAME} to SQL"
 echo "Writing to $TEMP_FILE"
 
 dbml2sql $DBML_FILE > $TEMP_FILE
 cd $MODELS_LOCATION || exit
 omm -m sqlalchemy $TEMP_FILE
-cd - &> /dev/null || exit
+cd "$CWD" || exit
