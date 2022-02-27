de65bb56bf1e01a6f1a4b4c17fa942cc02fa0cb7
isaak-a <isaakranderson@gmail.com>
2022-02-27 14:08:27 -0500

    ingest script for loading charge data

---
+++ ingest/charges.py
@@ -0,0 +1,115 @@
+import logging
+
+from sqlalchemy.engine import Engine
+from distributed import Client as DaskClient
+import dask.dataframe as dd
+import numpy as np
+
+from db.utils import DATABASE_URI, create_db_engine
+from db.dask_utils import load_to_db
+from db.models import Charges
+
+
+logger = logging.getLogger(__name__)
+
+
+def read_raw_data() -> dd.DataFrame:
+    logger.info('Reading raw data into Dask')
+    return dd.read_sql_table(
+        'expunge',
+        DATABASE_URI,
+        index_col='person_id'
+    )
+
+
+def rename_columns(ddf: dd.DataFrame) -> dd.DataFrame:
+    column_map = {
+        'HearingDate': 'hearing_date',
+        'CodeSection': 'code_section',
+        'ChargeType': 'charge_type',
+        'Class': 'charge_class',
+        'DispositionCode': 'disposition_code',
+        'Plea': 'plea',
+        'Race': 'race',
+        'Sex': 'sex',
+        'fips': 'fips'
+    }
+    return ddf.rename(columns=column_map)
+
+
+def filter_hearing_dates(ddf: dd.DataFrame) -> dd.DataFrame:
+    ddf['hearing_date'] = ddf['hearing_date'].astype('datetime64[ns]')
+    # TODO: Investigate valid reasons for future hearing dates. 
+    return ddf[ddf['hearing_date'] <= np.datetime64('today')]
+
+
+def standardize_race(ddf: dd.DataFrame) -> dd.DataFrame:
+    race: dd.Series = ddf['race'].str.upper()
+    ddf['race'] = (
+        ddf['race']
+            .mask(race.str.contains('BLACK'), 'Black')
+            .mask(
+                race.str.contains('WHITE')
+                | race.str.contains('CAUCASIAN'),
+                'White'
+            )
+            .mask(race.str.contains(
+                # https://regex101.com/r/nJazne/1
+                r"(?<![A-Z])ASIAN", regex=True),
+                'Asian or Pacific Islander'
+            )
+            .mask(
+                race.str.contains('AMERICAN INDIAN')
+                | race.str.contains('NATIVE'),
+                'American Indian or Alaskan Native'
+            )
+            .mask(
+                race.str.contains('OTHER')
+                | race.str.contains('MISSING')
+                | race.str.contains('UNKNOWN'),
+                'Unknown'
+            )
+    )
+    return ddf
+
+
+def fix_fips_codes(ddf: dd.DataFrame) -> dd.DataFrame:
+    ddf['fips'] = (ddf['fips'].astype(str)
+                              .str.pad(5, fillchar='0'))
+    return ddf
+
+
+def clean_charge_data(ddf: dd.DataFrame) -> dd.DataFrame:
+    logger.info("Building task graph to clean data")
+    return (
+        ddf.pipe(rename_columns)
+           .pipe(filter_hearing_dates)
+           .pipe(standardize_race)
+           .pipe(fix_fips_codes)
+    )
+
+
+def load_charges(engine: Engine, npartitions: None):
+    ddf = dd.read_sql_table(
+        'expunge',
+        DATABASE_URI,
+        index_col='person_id',
+        npartitions=npartitions
+    )
+    ddf = clean_charge_data(ddf)
+    load_to_db(
+        ddf, 
+        target_model=Charges, 
+        engine=engine
+    )
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s - %(module)s.py]: %(message)s',
+        datefmt='%H:%M:%S'
+    )
+    DaskClient()
+    engine = create_db_engine()
+    load_charges(engine)
