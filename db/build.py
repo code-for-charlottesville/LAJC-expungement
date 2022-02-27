5664d504ec355a3645fdbe423b261abdaeaef50a
isaak-a <isaakranderson@gmail.com>
2022-02-27 14:19:02 -0500

    Add cascading deletes for foreign keys

--- db/build.py
+++ db/build.py
@@ -1,9 +1,9 @@
 """Running this script directly builds the database tables in PostGres"""
 
-from db.utils import get_db_engine
+from db.utils import create_db_engine
 from db.models import Base
 
 
 if __name__ == '__main__':
-    engine = get_db_engine(echo=True)
+    engine = create_db_engine(echo=True)
     Base.metadata.create_all(engine)
